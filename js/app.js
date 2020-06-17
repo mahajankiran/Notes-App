44be14d7ffe034a9ae4f0f8f7c6b94e0c6ffffb6
kiran <mahajankiran020@gmail.com>
2020-06-17 21:20:01 +0530

    Added the segregation notes by search feature

--- js/app.js
+++ js/app.js
@@ -75,4 +75,27 @@ function deleteNote(index)
     
 }
 
+let search=document.getElementById('searchTxt');
 
+search.addEventListener("input",function(){
+    let inputVal=search.value;
+    
+    let noteCards=document.getElementsByClassName('noteCard');
+
+    Array.from(noteCards).forEach((element)=>{
+
+        let cardTxt=element.getElementsByTagName("p")[0].innerText;
+        if(cardTxt.includes(inputVal))
+        {
+            element.style.display="block";
+        }
+        else
+        {
+            element.style.display="none";
+        }
+
+
+
+    })
+
+})
